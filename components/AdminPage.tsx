d874f07fd340f68df574ea5fe675be4b88af5da1
geamed <robuxwin24@gmail.com>
2025-07-27 15:09:51 +0000

    chore: remove unused Modal import from AdminPage to fix Netlify build error

--- components/AdminPage.tsx
+++ components/AdminPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { AdminWithdrawalRequest, WithdrawalRequest, NotificationType } from '../types';
-import Modal from '../components/Modal';
+
 
 interface AdminPageProps {
   getAllRequests: () => Promise<AdminWithdrawalRequest[]>;
