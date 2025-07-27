9391b33a0f71f800f9f597ff1c73ab66d1535bcd
geamed <robuxwin24@gmail.com>
2025-07-27 16:53:07 +0000

    fix: wrap all pages in AuthProvider to ensure useAuth works everywhere

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from 'next/app';
 import '../index.css';
+import { AuthProvider } from '../contexts/AuthContext';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
